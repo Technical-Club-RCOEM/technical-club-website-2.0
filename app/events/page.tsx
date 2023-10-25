8dcedc8c34d523161a59450cc2b8ea6e028b7c9d
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-10-25 22:56:03 +0530

    events page:lacks responsiveness and fonts

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,12 +3,15 @@ import Image from 'next/image';
 
 export default function Events() {
 	return (
-		
-			<div className='flex justify-center mt-20'>
-				<Image src="/img1.jpg" className="ml-2" alt="Logo" width={240} height={300} priority />
-				<Image src="/img1.jpg" className="ml-2" alt="Logo" width={240} height={300} priority />
-				<Image src="/img1.jpg" className="ml-2" alt="Logo" width={240} height={300} priority />
+		<>
+			<div className='flex justify-center mt-12'>
+				<Image src="/img1.jpg" className="ml-2  border-black border-4 rounded-[150px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2 relative top-16 border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2  border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
 			</div>
+
+			<p className='events-head'>EVENTS</p>
+		</>	
 		);
 		
 }
