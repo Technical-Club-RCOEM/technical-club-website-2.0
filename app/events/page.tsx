29aebb4aa8962086af5ff0c4e838fbb553b43a4e
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-10-25 21:57:29 +0530

    brench create

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,3 +1,14 @@
+import img1 from '/img1.jpg'
+import Image from 'next/image';
+
 export default function Events() {
-	return <div>Hi Events</div>;
+	return (
+		
+			<div className='flex justify-center mt-20'>
+				<Image src="/img1.jpg" className="ml-2" alt="Logo" width={240} height={300} priority />
+				<Image src="/img1.jpg" className="ml-2" alt="Logo" width={240} height={300} priority />
+				<Image src="/img1.jpg" className="ml-2" alt="Logo" width={240} height={300} priority />
+			</div>
+		);
+		
 }
