cfd8b0ec05bad0700af50b545db8b9cbbcef621c
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-10-25 11:15:47 +0530

    chore: start the work

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import Image from 'next/image';
 
 export default function Home() {
-	return <main>Hello</main>;
+	return (
+		<>
+			<div className="flex w-full items-center justify-between bg-slate-100">
+				<p className="ml-32 text-7xl">
+					It's
+					<br />
+					Technical!
+				</p>
+				<Image src="/logo asset.svg" className="ml-4" alt="Logo" width={800} height={800} priority />
+			</div>
+		</>
+	);
 }
