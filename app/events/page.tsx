001e1797f3fc41a3e86bc742c6e15c620bef3ec5
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2023-10-28 21:19:44 +0530

    add Jayesh work (#1)

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,3 +1,17 @@
+import img1 from '/img1.jpg'
+import Image from 'next/image';
+
 export default function Events() {
-	return <div>Hi Events</div>;
+	return (
+		<>
+			<div className='flex justify-center mt-12'>
+				<Image src="/img1.jpg" className="ml-2  border-black border-4 rounded-[150px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2 relative top-16 border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2  border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+			</div>
+
+			<p className='events-head'>EVENTS</p>
+		</>	
+		);
+		
 }
