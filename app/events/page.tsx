bce87ba22d57fb4884fd672d4e9873e906d7a5fb
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-10-28 23:52:12 +0530

    update of event box

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,17 +1,23 @@
+import Box from './box';
 import img1 from '/img1.jpg'
 import Image from 'next/image';
 
 export default function Events() {
 	return (
-		<>
+		<div className='h-full w-full'>
+			<div className='h-screen w-full'>
 			<div className='flex justify-center mt-12'>
-				<Image src="/img1.jpg" className="ml-2  border-black border-4 rounded-[150px]" alt="Logo" width={350} height={500} priority />
-				<Image src="/img1.jpg" className="ml-2 relative top-16 border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
-				<Image src="/img1.jpg" className="ml-2  border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 rounded-[150px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2 events-title-image relative top-16 border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+				<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
 			</div>
 
 			<p className='events-head'>EVENTS</p>
-		</>	
+			</div>
+			<Box />
+
+			Yo
+		</div>	
 		);
 		
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ export default {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
+			colors: {
+				'tc-blue': '#04284E',
+			},
 		},
 	},
 	darkMode: 'class',
