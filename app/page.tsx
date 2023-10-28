dfc55e333daa4c7790936dc5ff88e0283f233074
pravin nandankar <pravinnandankar03@gmail.com>
2023-10-28 11:31:57 +0530

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import About from './components/About';
 export default function Home() {
 	return (
 		<>
-			<div className="flex w-full items-center justify-between">
-				<p className="ml-40 text-9xl">
+			<div className="md:flex w-full items-center justify-center overflow-hidden">
+				<p className="md:ml-40 md:text-9xl text-5xl ml-8 mt-8 ">
 					It's
 					<br />
 					Technical!
 				</p>
-				<Image src="/logo asset.svg" className="ml-4" alt="Logo" width={800} height={800} priority />
+				<Image src="/logo asset.svg" className="md:ml-6 relative bottom-16 left-16" alt="Logo" width={800} height={800} priority />
 			</div>
 			<About/>
 		</>
