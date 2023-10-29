844d87c6ff9dd40c662e0acf80f576f72125f1b3
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-10-29 12:01:02 +0530

    EVENTS FULL RESPONSIVE DONE FEW GLITCHES AND FONTS LEFT

--- app/events/box.tsx
+++ app/events/box.tsx
@@ -5,11 +5,12 @@ import Image from 'next/image';
 export default function Box () {
     return(
         <>
-            <div className="flex justify-center h-full">
-                <div className="bg-tc-blue min-h-[500px] w-9/12 rounded-b-[100px]">
-                    <div className='text-white pl-10 pt-12 pb-10 flex gap-6 pr-10'>
+            <div className="flex justify-center h-full w-full">
+                <div className="bg-tc-blue events-box rounded-b-[100px]">
+                    <div className='text-white pl-10 pt-12 pb-10 gap-6 pr-10 flex flex-col lg:flex-row'>
                         <Image src="/photo1.jpeg" alt="Logo" className='border-white border-8 rounded-bl-[80px] rounded-tr-[80px]' width={400} height={480} priority />
-                        <p > Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero esse fugit, soluta excepturi necessitatibus nemo eaque distinctio nisi, autem, eos assumenda accusamus fugiat quisquam ipsa. Voluptatibus, adipisci iure fugiat voluptatum sunt totam amet deserunt maiores natus! Voluptas laboriosam alias nisi mollitia veritatis ipsa optio earum dolorem? Dicta omnis maxime incidunt unde, quibusdam molestiae nobis, similique quia ut tempore necessitatibus. Sapiente enim, aut corrupti deserunt velit provident tempora esse debitis cumque nostrum perspiciatis alias nam modi doloribus quae facere excepturi commodi deleniti sint itaque hic. Consectetur porro blanditiis veniam! Dolorum labore cupiditate, pariatur ad iste possimus deserunt omnis ullam sapiente. </p>
+                        <p className='text-center'>HEADING</p>
+                        <p className='text-center'>DESC</p>
                     </div>
                 </div>
             </div>
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,21 +3,34 @@ import img1 from '/img1.jpg'
 import Image from 'next/image';
 
 export default function Events() {
+	const arr: { name: string; img: string; desc:string; }[] = [
+        {
+            name : "Robodrive",
+            img : "/html.png",
+			desc : "lorem"
+		},
+        
+        
+    ]
 	return (
-		<div className='h-full w-full'>
-			<div className='h-screen w-full'>
-			<div className='flex justify-center mt-12'>
-				<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 rounded-[150px]" alt="Logo" width={350} height={500} priority />
-				<Image src="/img1.jpg" className="ml-2 events-title-image relative top-16 border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
-				<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 rounded-tl-[100px] rounded-tr-[100px] rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
-			</div>
+		<div className=' h-full w-full'>
+			<div className='slider'>
+				<div className='h-screen w-full overflow-hidden'>
+					<div className='events-slider flex justify-center mt-12 overflow-hidden'>
+						<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 lg:rounded-[150px] " alt="Logo" width={350} height={500} priority />
+						<Image src="/img1.jpg" className="ml-2 events-title-image lg:relative lg:top-16 border-black border-4 lg:rounded-tl-[100px] lg:rounded-tr-[100px] lg:rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+						<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 lg:rounded-tl-[100px] lg:rounded-tr-[100px] lg:rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
+						<Image src="/img1.jpg" className="ml-2 events-title-image border-black border-4 lg:rounded-tl-[100px] lg:rounded-tr-[100px] lg:rounded-br-[100px]" alt="Logo" width={350} height={500} priority />
 
-			<p className='events-head'>EVENTS</p>
-			</div>
-			<Box />
+					</div>
 
-			Yo
-		</div>	
+					<p className='events-head'>EVENTS</p>
+				</div>
+			</div>
+			<Box></Box>
+
+			
+		</div>		
 		);
 		
 }
