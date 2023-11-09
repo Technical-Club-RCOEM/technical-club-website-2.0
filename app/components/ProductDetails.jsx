f7911026f94ef100abf01bfdc2548dc7c8946e1f
pravin nandankar <pravinnandankar03@gmail.com>
2023-11-09 14:26:31 +0530

    domains

--- app/components/ProductDetails.jsx
+++ app/components/ProductDetails.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const ProductDetails = ({ selectedProduct }) => {
   return (
-    <div className="w-full flex justify-center items-center overflow-hidden bg-[#091f36] p-10">
+    <div className="w-full flex justify-center items-center overflow-hidden bg-[#091f36] p-10 text-white ">
       {selectedProduct && (
-        <div className="w-max h-72 flex justify-center rounded-sm items-center gap-10 border p-10 ">
-          <Image src={selectedProduct.img} className="" alt="Logo" width={400} height={400} priority />
-          <div className="flex w-96 flex-col justify-center items-start gap-5">
+        <div className="sm:w-max h-72 flex flex-col sm:flex-row justify-center rounded-sm items-center gap-10 sm:border sm:p-10 ">
+          <Image src={selectedProduct.img} className="w-1/2 sm:w-96" alt="Logo" width={400} height={400} priority />
+          <div className="flex w-96 flex-col justify-center items-center sm:items-start gap-5">
           <h2 className="text-3xl">{selectedProduct.name}</h2>
-          <p className="font-bold-100">{selectedProduct.info}</p>
+          <p className="font-bold-100 text-center sm:text-start">{selectedProduct.info}</p>
           </div>
         </div>
       )}
--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const ProductList = ({ products, onItemClick }) => {
 	return (
-		<div className="w-full  flex justify-center items-center z-10 p-10 cursor-pointer bg-[#091f36] text-white">
-			<ul className="flex border-2 rounded gap-24 text-xl z-10 p-5">
+		<div className="w-full  flex justify-center items-center z-10 p-0 pb-5 sm:p-10 cursor-pointer bg-[#091f36] text-white">
+			<ul className="flex border-2 rounded gap-0 sm:gap-24 text-base sm:text-xl z-10 p-1 sm:p-5">
 				{products.map((product) => (
-					<li key={product.id} onClick={() => onItemClick(product)} className='mr-8 hover:text-gray-300'>
+					<li key={product.id} onClick={() => onItemClick(product)} className='text-center mr-1 sm:mr-8 hover:text-gray-300'>
 						{product.name}
 					</li>
 				))}
