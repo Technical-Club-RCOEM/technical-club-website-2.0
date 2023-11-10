09c1da392d633027e85a3433d0302c4ee531cf7d
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-11-10 16:42:52 +0530

    chore: fixed achievements page

--- app/achievements/page.tsx
+++ app/achievements/page.tsx
@@ -1,94 +1,103 @@
-'use client'
-import Image from 'next/image';
-import { oswald } from '@/app/fonts';
+'use client';
+import { Button } from '@nextui-org/react';
 import Box from '../events/box';
 import { BoxProps } from '../events/box';
-import React , {useState} from 'react'
-import '../events/events.css'
-
+import React, { useState } from 'react';
+import '../events/events.css';
+import img1 from '../components/img/img1.jpg';
+import photo1 from '../components/img/photo1.jpeg';
 
 const Achievements = () => {
-  const [curr , setCurr] = useState(0);
+	const [curr, setCurr] = useState(0);
 
-	const next = () => {
-		setCurr(curr + 1);
-	}
-	const prev = () => {
-		setCurr(curr - 1);
-	}
-  const arr: BoxProps[] = [
+	const arr: BoxProps[] = [
 		{
 			name: 'Hackanova 2.0 TCET Open Innovation',
-			img: '/img1.jpg',
+			img: img1,
 			description: 'Winner ',
 		},
 		{
 			name: 'CodeFury ',
-			img: '/img1.jpeg',
+			img: img1,
 			description: 'Winner',
 		},
 		{
 			name: 'Autocoders GCOEM ',
-			img: '/photo1.jpeg',
+			img: photo1,
 			description: 'Winner ',
 		},
 		{
 			name: ' IITB Techfest Zonals code',
-			img: '/photo1.jpeg',
+			img: photo1,
 			description: '3rd ',
 		},
 		{
 			name: ' Designex 2.0 VIT-AP',
-			img: '/photo1.jpeg',
+			img: photo1,
 			description: 'Winner ',
 		},
 		{
 			name: ' IISF-SIF (MANIT Bhopal) ',
-			img: '/photo1.jpeg',
+			img: photo1,
 			description: 'AIR 13th  ',
 		},
 		{
 			name: ' IDRL S7Ep6 @ Amalthea 2022, IIT Gandhinagar ',
-			img: '/photo1.jpeg',
+			img: photo1,
 			description: ' 7th position  ',
 		},
 		{
 			name: ' IDRL S7Ep8 @ Technex-23, SPVCET, Nagpur  ',
-			img: '/photo1.jpeg',
+			img: photo1,
 			description: '7th Position',
 		},
-		
 	];
-  return (
-    <div className='w-full h-full'>
-      <div className='w-screen h-screen flex flex-col items-center justify-center -mt-7 '>
-        <div className='bg-tc-blue w-9/12 h-3/4 rounded-[100px]  achieve-bg relative'>
-        </div>
-          <p className={'absolute lg:top-1/3 lg:left-[25vw] text-3xl lg:text-[100px]  text-white achieve-head'}>Achievements</p>
-      </div> 
 
-      <div className="relative flex lg:gap-40 overflow-x-hidden" >
-				
-				<div className='flex transition-transform ease-out duration-500 pb-10'  style={{ transform: `translateX(-${curr * (100 / arr.length)}%)` }}>
+	const next = () => {
+		setCurr((prev) => {
+			if (prev + 1 === arr.length) return 0;
+			return prev + 1;
+		});
+	};
+	const prev = () => {
+		setCurr((prev) => {
+			if (prev - 1 === -1) return arr.length - 1;
+			return prev - 1;
+		});
+	};
+
+	return (
+		<div className="h-full w-full">
+			<div className="-mt-7 flex h-screen w-screen flex-col items-center justify-center ">
+				<div className="achieve-bg relative h-3/4 w-9/12  rounded-[100px] bg-tc-blue"></div>
+				<p className={'achieve-head absolute text-3xl text-white lg:left-[25vw]  lg:top-1/3 lg:text-[100px]'}>
+					Achievements
+				</p>
+			</div>
+
+			<div className="relative flex overflow-x-hidden lg:gap-40">
+				<div
+					className="flex pb-10 transition-transform duration-500 ease-out"
+					style={{ transform: `translateX(-${curr * (100 / arr.length)}%)` }}
+				>
 					{arr.map((a) => {
 						return <Box {...a} />;
 					})}
 				</div>
-			
-				<button className='absolute left-2 p-1 lg:top-60 rounded-md bg-slate-600 top-48 h-10 text-white' onClick={prev}>
-				Prev
-			</button>
 
-			<button className='absolute right-4 lg:top-60 p-1 rounded-md  top-48 h-10 text-white bg-slate-600' onClick={next}>
-				Next
-			</button>
-			
+				<Button className="absolute left-2 top-48 h-10 rounded-md bg-slate-600 p-1 text-white lg:top-60" onClick={prev}>
+					Prev
+				</Button>
+
+				<Button
+					className="absolute right-4 top-48 h-10 rounded-md  bg-slate-600 p-1 text-white lg:top-60"
+					onClick={next}
+				>
+					Next
+				</Button>
 			</div>
+		</div>
+	);
+};
 
-
-
-    </div>
-  )
-}
-
-export default Achievements
+export default Achievements;
