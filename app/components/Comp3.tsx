9ef4c2204c119bf0aee8b7a68339ed73fc543457
EvolutionX <85353424+EvolutionX-10@users.noreply.github.com>
2023-11-10 18:12:20 +0530

    chore: reticulating splines...
    permalink: http://whatthecommit.com/e62c4158a5e73beb026ef7525568060c

--- app/components/Comp3.tsx
+++ app/components/Comp3.tsx
@@ -7,7 +7,7 @@ export default function Component() {
 	return (
 		<div className="relative mt-32 flex h-60 w-2/3 items-center justify-center max-md:w-[90vw]">
 			<Image src={shape} className="" alt="Logo" width={700} height={700} priority />
-			<div className="absolute -top-24 left-36 max-md:-top-2 max-md:left-12">
+			<div className="absolute -top-24 left-[15vw] max-md:-top-2 max-md:left-12">
 				<div className="photo1 absolute aspect-[9/16] w-44 max-md:w-24"></div>
 				<div className="absolute -bottom-[21rem] w-44 text-center text-sm text-black max-md:-bottom-[12rem] max-md:w-24">
 					Harsheet
